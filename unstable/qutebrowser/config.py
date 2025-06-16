01b50925ce7a2b1b150b10f5b418e96953f260ad
lpj <lpj8866@gmail.com>
2025-06-16 21:37:12 +0800

    update

--- unstable/qutebrowser/config.py
+++ unstable/qutebrowser/config.py
@@ -9,7 +9,7 @@ c.content.proxy = "http://127.0.0.1:1080"
 c.auto_save.session = True
 
 # dark mode
-c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.enabled = False
 
 # c.url.searchengines = {
 # if you use duckduckgo, you can make use of its built in bangs,
@@ -18,10 +18,10 @@ c.colors.webpage.darkmode.enabled = True
 
 # key-binding changes
 c.tabs.show = "multiple"
-c.statusbar.show = "never"
 c.tabs.position = "top"
+c.statusbar.show = "always"
 config.bind('tH', 'config-cycle tabs.show multiple never')
-config.bind('sH', 'config-cycle statusbar.show always never')
 config.bind('tT', 'config-cycle tabs.position top left')
+config.bind('sH', 'config-cycle statusbar.show always in-mode')
 config.bind('h', 'history')
 
