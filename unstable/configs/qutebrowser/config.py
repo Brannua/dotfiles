19b55ca73d8010b93df76a7edf6a3dad3701d678
lpj <lpj8866@gmail.com>
2025-06-20 22:53:48 +0800

    update

--- unstable/configs/qutebrowser/config.py
+++ unstable/configs/qutebrowser/config.py
@@ -5,18 +5,8 @@ config.load_autoconfig(False)
 # proxy
 c.content.proxy = "http://127.0.0.1:1080"
 
-# save tabs on quit/restart
-c.auto_save.session = False
-# c.auto_save.session = True
-
 # dark mode
-# c.colors.webpage.darkmode.enabled = False
-c.colors.webpage.darkmode.enabled = True
-
-# c.url.searchengines = {
-# if you use duckduckgo, you can make use of its built in bangs,
-# https://duckduckgo.com/bangs
-# }
+c.colors.webpage.darkmode.enabled = False
 
 # key-binding changes
 c.tabs.show = "multiple"
