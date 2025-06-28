8c7f5f29238f69c8109b35eac94c917380354876
lpj <374453156@qq.com>
2025-06-28 21:05:16 +0800

    update

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -4,3 +4,15 @@
 
 fish_vi_key_bindings
 
+if test -f ~/.config/bash/rc.alias.fish
+  source ~/.config/bash/rc.alias.fish
+end
+
+if test -f ~/.config/fzf/config.fish
+  source ~/.config/fzf/config.fish
+end
+
+if test -f ~/.config/yazi/functions/wrapper.fish
+  source ~/.config/yazi/functions/wrapper.fish
+end
+
