1f0f981c6bea242b99ce321a2b5c788104f7905b
lpj <374453156@qq.com>
2025-07-09 20:04:39 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,6 @@
 fish_vi_key_bindings
 
-if test -f ~/.config/bash/alias.sh
-  source ~/.config/bash/alias.sh
-end
-
-if test -f ~/.config/yazi/functions/wrapper.fish
-  source ~/.config/yazi/functions/wrapper.fish
-end
-
-if test -f ~/.config/fzf/config.fish
-  source ~/.config/fzf/config.fish
-end
+test -f ~/.config/bash/alias.sh; and source ~/.config/bash/alias.sh
+test -f ~/.config/yazi/functions/wrapper.fish; and source ~/.config/yazi/functions/wrapper.fish
+test -f ~/.config/fzf/config.fish; and source ~/.config/fzf/config.fish
 
