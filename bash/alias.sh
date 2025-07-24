271a57feceb2edc3df07b51b1c8367532ecf2432
lpj <374453156@qq.com>
2025-07-24 19:51:07 +0800

    update

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,20 +1,28 @@
 #
 # alias for (bash,zsh,fish,...)
+# Some useful tools: trans, yt-dlp
 #
 
 alias q=exit
 alias c=clear
 
+alias rm="rm -i"
+alias mv="mv -i"
+alias cp="cp -i"
+
+alias g=git
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gd="git diff"
+alias gl="git log --all --graph"
+
 alias ls=eza
 alias l="ls -alh"
-
 alias f=fish
 alias s=fastfetch
 
-# alias fy=trans
-# alias dl=yt-dlp
-
-# proxy
 alias proxy-on="export all_proxy='socks5://127.0.0.1:1080'"
 alias proxy-off="unset all_proxy"
 
