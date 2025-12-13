e70dc27e82773b929bdb82da56ab836d12fbcdad
lpj <374453156@qq.com>
2025-12-13 20:04:42 +0800

    update

--- bash/alias.sh
+++ bash/alias.sh
@@ -28,6 +28,7 @@ alias l="ls -alh"
 alias f=fish
 alias s=fastfetch
 alias b=bluetui
+alias p=pulsemixer
 
 alias proxy-on="export all_proxy='socks5://127.0.0.1:1080'"
 alias proxy-off="unset all_proxy"
