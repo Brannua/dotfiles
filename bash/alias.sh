ab3ef11e575c06697293959274c397392ad97fc5
lpj <374453156@qq.com>
2026-04-07 13:54:11 +0800

    update

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,54 +1,48 @@
-#!/usr/bin/sh
-
-#
-# alias for shell(bash,fish,...)
-# Some useful tools are under below.
-# dysk: https://github.com/Canop/dysk
-# tlrc: https://github.com/tldr-pages/tlrc
-# acpi: https://sourceforge.net/projects/acpiclient/files/acpiclient
-#
-
-alias ..="cd .."
-alias k="i3-msg exit"
-
-alias c=clear
-alias open=xdg-open
-
-alias rm="rm -i"
-alias mv="mv -i"
-alias cp="cp -i"
-
-alias grep="grep --ignore-case"
-
-alias g=git
-alias gs="git status"
-alias ga="git add"
-alias gc="git commit"
-alias gp="git push"
-alias gd="git diff"
-alias gl="git log --all --graph"
-
-alias ls=eza
-alias sl=eza
-alias l="ls -alh"
-alias f=fish
-alias s=fastfetch
-alias b=bluetui
-alias p=pulsemixer
-
 alias proxy-on="export all_proxy='socks5://127.0.0.1:10808'"
 alias proxy-off="unset all_proxy"
 
 alias extMoni="xrandr --output DP-1 --auto --primary --output eDP-1 --off"
 alias intMoni="xrandr --output eDP-1 --auto --primary --output DP-1 --off"
 
+# ---
+
+alias ls=eza
+alias sl=eza
+alias l="eza -alh"
+
+alias c=clear
+
+alias ..="cd .."
+
+alias rm="rm -i"
+alias mv="mv -i"
+alias cp="cp -i"
+
+# ---
+
+# some git commands
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+
+# ---
+
+# some useful programs
+alias o=xdg-open
+alias s=fastfetch
+alias b=bluetui
+alias p=pulsemixer
 alias pk=pkill
-alias py=python
-alias rsync="rsync --progress"
 
 # playlist of bilibili: https://space.bilibili.com/37376741/lists/134983
 # singleVideo of bilibili: https://www.bilibili.com/video/BV1Jq4y1C77N
 alias dl=yt-dlp
 
+# ---
+
+# some useful options
+alias grep="grep --ignore-case"
+alias rsync="rsync --progress"
 alias date="TZ='Asia/Shanghai' date"
 
